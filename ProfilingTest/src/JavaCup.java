075a7f7a24c009515444a93c3000162c3937e372
mostafa <mostafa.hosseini788@gmail.com>
2025-05-08 19:00:00 +0330

    modify JavaCup.java

--- ProfilingTest/src/JavaCup.java
+++ ProfilingTest/src/JavaCup.java
@@ -23,11 +23,12 @@ public class JavaCup {
         else { System.out.println("NO"); }
     }
     public static void temp() {
-        ArrayList a = new ArrayList();
-        for (int i = 0; i < 10000; i++)
-        {
+        int[] a = new int[10000 * 20000];
+        int cpunter = 0;
+        for (int i = 0; i < 10000; i++) {
             for (int j = 0; j < 20000; j++) {
-                a.add(i + j);
+                a[cpunter] = i + j;
+                cpunter += 1;
             }
         }
     }
